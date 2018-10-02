24615cd74d1490500cad0fc989e14fff91d5393b
b-sonter <40931305+b-sonter@users.noreply.github.com>
2018-10-02 10:17:46 +1000

    create function names for channels
    Create all the function names for groups and channels

--- chat-app/src/app/home/home.component.ts
+++ chat-app/src/app/home/home.component.ts
@@ -23,27 +23,54 @@ export class HomeComponent implements OnInit {
       // User has not logged in, reroute to login
       this.router.navigate(['/login']);
     } else {
+      //log the user in sessionStorage
       let userlog = sessionStorage.getItem('userlog');
       this.userlog = userlog;
+      //log the user permissions in sessionStorage
       let permslog = sessionStorage.getItem('permslog');
       this.permslog = permslog;
     }
   }
 
 
-
+  //log out of the chat-app
   logout(){
     sessionStorage.clear();
+    //redirect to login page
     this.router.navigate(['/login']);
   }
 
+  //go to the section to create a new user
   goCreate(){
+  //redirect to create page
     this.router.navigate(['/create']);
   }
 
+  //go to the delete user section
   goDelete(){
+    //redirect to delete page
     this.router.navigate(['/delete']);
   }
+
+  //create a new groups
+  createGroup(event){
+
+  }
+
+  //get list of groups from database
+  getGroups(){
+    
+  }
+
+  //determine which group is currently open
+  openGroup(name){
+
+  }
+
+  //get channels for the groups
+  getChannels(){
+
+}
 
 
 }
